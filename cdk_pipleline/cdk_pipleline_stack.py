bc6f5e65d83ebf32ee25dcc94fe93e0b2004114d
Brijesh Mangroliya <Brijesh.Mangroliya@scalecapacity.com>
2024-08-08 12:22:29 -0400

    testing deploy to MLRND and scai-stage

--- cdk_pipleline/cdk_pipleline_stack.py
+++ cdk_pipleline/cdk_pipleline_stack.py
@@ -1,6 +1,7 @@
 import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
+from cdk_pipleline.cdk_pipleline_app_stage import MyPipelineAppStage
 
 class CdkPipelineStack(cdk.Stack):
 
@@ -10,9 +11,24 @@ class CdkPipelineStack(cdk.Stack):
         pipeline =  CodePipeline(self, "Pipeline",
                         pipeline_name="testCdkPipelineCrossAccount",
                         synth=ShellStep("Synth",
-                            input=CodePipelineSource.connection("bhmangroliya/cdk-pipeline", "main", connection_arn='arn:aws:codeconnections:us-east-1:043037169024:connection/1fa5c7b0-2503-4dc2-b9cc-3501447864cb'),
+                            input=CodePipelineSource.connection("bhmangroliya/cdk-pipleline", "main", connection_arn='arn:aws:codeconnections:us-east-1:043037169024:connection/1fa5c7b0-2503-4dc2-b9cc-3501447864cb'),
                             commands=["npm install -g aws-cdk",
                                 "python -m pip install -r requirements.txt",
                                 "cdk synth"]
-                        )
+                        ),
+                        publish_assets_in_parallel=False
                     )
+        
+        deploy_to_dev= pipeline.add_stage(
+                        MyPipelineAppStage(
+                            self, "deployToDev",
+                            env=cdk.Environment(account="043037169024", region="us-east-1")))
+        
+        deploy_to_test = pipeline.add_stage(
+            MyPipelineAppStage(
+                self,
+                "deployToTest",
+                env=cdk.Environment(account="451949150028", region="us-east-1"),
+            ),
+            pre=[cdk.pipelines.ManualApprovalStep("approval")],
+        )
